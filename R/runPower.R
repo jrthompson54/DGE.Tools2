0365dd29b51685175209a5e4ff8750ce47009e03
thompj27 <john.thompson@bms.com>
2017-04-18 09:38:54 -0400

    drop plotBCV.R (now bcv option in plotDisp)

--- R/runPower.R
+++ R/runPower.R
@@ -116,6 +116,7 @@ runPower <- function(counts, designMatrix,
       ggtitle("ROC curves") +
       xlab("\nFDR") +
       ylab("Power") +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
       theme_gray(18)
 
     result$ROC <- roc
