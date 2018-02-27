660500767ea43bcfb3f3659c81dc36d36435e7b4
thompj27 <john.thompson@bms.com>
2018-02-27 13:00:56 -0500

    added new arguments to checkSex

--- R/checkSex.R
+++ R/checkSex.R
@@ -8,12 +8,13 @@
 #'
 #' @param dgeObj A DGEobj (Required)
 #' @param species One of "human", "mouse", "rat"
-#' @param sexCol Name of the sex annotation column in the design table within the dgeObj
-#' @param labelCol Name of the design column to use to label points
-#' @param showLables Set TRUE to label points with labelcol (default = FALSE)
-#' @param chrX Name of the X chromosome in the gene annotation within the geneData object (Default = "X")
-#' @param chrY Name of the Y chromosome in the gene annotation within the geneData object (Default = "Y")
+#' @param sexCol Character string name of the sex annotation column in the design table within the dgeObj (optional)
+#' @param labelCol Character string name of the design column to use to label points with ggrepel (optional if showLabels=FALSE)
+#' @param showLabels Set TRUE to turn on ggrepel labels (default = FALSE)
+#' @param chrX Character string name of the X chromosome in the gene annotation within the geneData object (Default = "X")
+#' @param chrY Character string name of the Y chromosome in the gene annotation within the geneData object (Default = "Y")
 #' @param baseFontSize Sets the base font size for ggplot themes (Default = 14)
+#' @param orig Set to FALSE to use your filtered DGEobj data.  Set to TRUE to use original unfiltered data (Default = TRUE)
 #'
 #' @return A ggplot object
 #'
@@ -22,15 +23,14 @@
 #' @import ggplot2 magrittr edgeR assertthat ggrepel ggiraph
 #'
 #' @export
-checkSex <- function(dgeObj, species, sexCol, labelCol, showLabels=FALSE, chrX = "X", chrY = "Y", baseFontSize=14) {
+checkSex <- function(dgeObj, species, sexCol, labelCol, showLabels=FALSE,
+                     chrX = "X", chrY = "Y", baseFontSize=14, orig=TRUE) {
 
   #convert this to a function in DGE.Tools2
   #input dgeObj, optionally specify x and y genes
   ##if genes not specified, pick highest expressed of each x and y gene
   assert_that(tolower(species) %in% c("human", "mouse", "rat"),
-              !missing(species),
-              !missing(sexCol),
-              !missing(labelCol))
+              !missing(species))
 
   if (tolower(species) == "rat") { #no XIST in Rat
     x <- .getTopExpressedGene(dgeObj, chr=chrX, orig=TRUE)
@@ -45,7 +45,12 @@ checkSex <- function(dgeObj, species, sexCol, labelCol, showLabels=FALSE, chrX =
   y <- .getTopExpressedGene(dgeObj, chr=chrY, orig=TRUE)
 
   #get data for the two x and y genes
-  log2CPM <- convertCounts(getItem(dgeObj, "counts_orig"), unit="cpm", log=TRUE, normalize="tmm")
+  if (orig){
+    log2CPM <- convertCounts(getItem(dgeObj, "counts_orig"), unit="cpm", log=TRUE, normalize="tmm")
+  } else {
+    log2CPM <- convertCounts(getItem(dgeObj, "counts"), unit="cpm", log=TRUE, normalize="tmm")
+  }
+
   idx <- rownames(log2CPM) %in% c(x$gene, y$gene)
   plotdat <- t(log2CPM[idx,]) %>%
     as.data.frame %>%
@@ -59,13 +64,21 @@ checkSex <- function(dgeObj, species, sexCol, labelCol, showLabels=FALSE, chrX =
 
   colnames(plotdat2) <- c("SampID", x$genename, y$genename, labelCol, sexCol)
 
-  sexPlot <- ggplot(plotdat2, aes_string(x=x$genename, y=y$genename, label=labelCol, color=sexCol)) +
+  if (!missing(labelCol) & !missing(sexCol)){
+    sexPlot <- ggplot(plotdat2, aes_string(x=x$genename, y=y$genename, label=labelCol, color=sexCol))
+  } else if (!missing(labelCol) & missing(sexCol)){
+    sexPlot <- ggplot(plotdat2, aes_string(x=x$genename, y=y$genename, label=labelCol))
+  } else if (missing(labelCol) & !missing(sexCol)){
+    sexPlot <- ggplot(plotdat2, aes_string(x=x$genename, y=y$genename, color=sexCol))
+  }
+
+  sexPlot <- sexPlot +
     geom_point(size=4, shape=21) +
     xlab(paste("X (", x$genename, ")", sep="")) +
     ylab(paste("Y (", y$genename, ")", sep="")) +
     ggtitle("Sex Determination Plot") +
     theme_grey()
-  if (showLabels == TRUE)
+  if (showLabels == TRUE & !missing(labelCol))
     sexPlot <-sexPlot + geom_label_repel (label.size=0.125)
 
   return(sexPlot)
@@ -78,6 +91,7 @@ checkSex <- function(dgeObj, species, sexCol, labelCol, showLabels=FALSE, chrX =
 #support function for Sex analysis
 .getChrDat <- function(dgeObj, chr, orig=FALSE){
   # chr can be one or more chromosome names
+  # Returns NULL if none found
   if (orig == TRUE) {
     geneData <- getItem(dgeObj, "geneData_orig")
     log2CPM <- convertCounts(getItem(dgeObj, "counts_orig"), unit="cpm", log=TRUE, normalize="tmm")
@@ -96,9 +110,12 @@ checkSex <- function(dgeObj, species, sexCol, labelCol, showLabels=FALSE, chrX =
 
   log2CPM_mat <- log2CPM[rownames(log2CPM) %in% rownames(geneData),]
 
-  return(log2CPM_mat)
+  if (is.null(nrow(log2CPM_mat))){
+    return(NULL)
+  } else {
+    return(log2CPM_mat)
+  }
 }
-
 
 .getTopExpressedGene <- function (dgeObj, chr, orig=FALSE){
   #get top mean expressed gene from given chromosome
@@ -112,6 +129,8 @@ checkSex <- function(dgeObj, species, sexCol, labelCol, showLabels=FALSE, chrX =
   #filter geneData to selected chr
   if (!is.null(chr))
     log2CPM_mat <- .getChrDat(dgeObj, chr=chr, orig=orig)
+  if (is.null(log2CPM_mat))
+    Stop ("No gene data found for specified chromosome.")
 
   meanLogCPM <- rowMeans(log2CPM_mat)
 
