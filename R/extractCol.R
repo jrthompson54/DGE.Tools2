02014ec3ffa09c1a93e93b3096547953dd6d3231
thompj27 <john.thompson@bms.com>
2018-05-29 14:39:41 -0400

    minor tweaks to help

--- R/extractCol.R
+++ R/extractCol.R
@@ -1,13 +1,15 @@
 ### Function extractCol ###
 #' Function  extractCol
 #'
-#' Take a list of dataframes where each dataframe has the same
+#' Take a named list of dataframes where each dataframe has the same
 #' column names (e.g. a list of topTable dataframes). Extract
 #' the named column from each dataframe and return a matrix.
 #'
 #' The common use case for this is to provide a list of topTable
 #' data frames and extract one column from each file to create
-#' a matrix of LogRatios or Pvalues. 
+#' a matrix of LogRatios or Pvalues.
+#'
+#' Note: all dataframes must have the same rownames/order
 #'
 #' @author John Thompson, \email{john.thompson@@bms.com}
 #' @keywords topTable
@@ -32,3 +34,27 @@ extractCol <- function(dflist, ColName){
   colnames(MyMatrix) = names(dflist)
   return(MyMatrix)
 }
+
+
+#' @import magrittr dplyr tibble
+#'
+#' @export
+extractCol2 <- function(dflist, ColName){
+  #support combining topTable data from different DGEobjs
+  for (i in 1:length(dflist)){
+
+    newdat <- dflist[[i]] %>%
+      rownames_to_column(var="rowid") %>%
+      select(rowid, ColName)
+
+    if (i == 1){
+      dat <- newdat
+    } else {
+      dat %<>% full_join(newdat, by="rowid")
+    }
+  }
+  dat %<>% column_to_rownames(var="rowid")
+  colnames(dat) <- names(dflist)
+  return(dat)
+}
+
