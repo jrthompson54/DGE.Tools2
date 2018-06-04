f5c61fb554e88ce27f5514c1e3fac6ed74ff1707
thompj27 <john.thompson@bms.com>
2018-06-04 00:01:20 -0400

    added function dflist_to_excel

--- test/buildAndInstall.R
+++ test/buildAndInstall.R
@@ -20,7 +20,6 @@ install_git("http://biogit.pri.bms.com/thompj27/DGE.Tools2", repos=BiocInstaller
 #for dev
 install_git("http://biogit.pri.bms.com/thompj27/DGE.Tools", branch="dev", repos=BiocInstaller::biocinstallRepos())
 
-
 #update BRAN
 library(bmsPackageTools)
 addPackageToRepo()
