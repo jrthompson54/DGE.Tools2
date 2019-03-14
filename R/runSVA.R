6497398c5fa315fcef39fab09dfc4045d41ddbc1
thompj27 <john.thompson@bms.com>
2019-03-13 21:59:52 -0400

    modified runSVA to enable running it before voom

--- R/runSVA.R
+++ R/runSVA.R
@@ -7,11 +7,13 @@
 #' analysis.
 #'
 #' @author John Thompson, \email{john.thompson@@bms.com}
-#' @keywords SVA SLOA
+#' @keywords SVA surrogate variable DGEobj
 #'
 #' @param dgeObj A DGEobj with normalized counts and a DesignMatrix.
+#' @param designMatrixName The itemName of the design matrix in DGEobj
+#' @param method method passed to num.sv. ["leek" or "be"; default = "leek"]
 #'
-#' @return dgeObj The DGEobj is returned containing a new design matrix.
+#' @return dgeObj The DGEobj is returned containing a new design matrix and an updated design table.
 #'
 #' @examples
 #' MyDgeObj = runSVA (MyDgeObj)
@@ -23,39 +25,44 @@
 #' @importFrom stats model.matrix
 #'
 #' @export
-runSVA<- function(dgeObj, designMatrixName){
+runSVA<- function(dgeObj, designMatrixName, method="leek"){
 
   assertthat::assert_that(!missing(dgeObj),
               !missing(designMatrixName),
-              class(dgeObj)[[1]] == "DGEobj",
-              class(designMatrixName)[[1]] == "character",
+              "DGEobj" %in% class(dgeObj),
+              "character" %in% class(designMatrixName),
+              tolower(method) %in% c("leek", "be"),
               with(dgeObj, exists("design")),
-              with(dgeObj, exists(designMatrixName)),
-              with(dgeObj, exists(paste(designMatrixName, "_Elist", sep="")))
+              with(dgeObj, exists(designMatrixName))
               )
+
+  method <- tolower(method)
 
   #Set up a NullFormula and DesignMatrix
   NullFormula = "~ 1"
   Design = DGEobj::getItem(dgeObj, "design")
   NullDesignMatrix = stats::model.matrix(as.formula(NullFormula), Design)
 
-  log2cpm <- DGEobj::getItem(dgeObj, paste(designMatrixName, "_Elist", sep=""))$E
+  # log2cpm <- DGEobj::getItem(dgeObj, paste(designMatrixName, "_Elist", sep=""))$E
+  log2cpm <- convertCounts(dgeObj$counts, unit="cpm", log=TRUE, normalize="tmm")
   designMatrix <- DGEobj::getItem(dgeObj, designMatrixName)
-  n.sv <- num.sv(log2cpm, designMatrix, method="leek")
+  n.sv <- num.sv(log2cpm, designMatrix, method=method)
   svobj <- sva::sva(log2cpm, designMatrix, NullDesignMatrix, n.sv=n.sv)
+
   #pull out the surrogate variables
   sv <- svobj$sv
 
   if (svobj$n.sv > 0) {
 
       #give them a colname
-      colnames(sv) <- paste("sva", 1:ncol(sv), sep="")
+      colnames(sv) <- paste("sv", 1:ncol(sv), sep="")
 
       # Add the SVA colums to the DesignMatrix
       designMatrix_SVA <- cbind (designMatrix, sv)
 
-      #capture output
+      #capture the function call
       FunArgs <- match.call()
+
       #save the svobj
       saveRDS(svobj, "svobj.RDS")
       dgeObj <- addItem(dgeObj, svobj, paste(designMatrixName, "_svobj", sep=""),
@@ -66,6 +73,10 @@ runSVA<- function(dgeObj, designMatrixName){
       dgeObj <- addItem(dgeObj, designMatrix_SVA, paste(designMatrixName, "_sva", sep=""),
                         "designMatrix", funArgs=FunArgs,
                         parent=designMatrixName)
+      #add the SV columns to the Design table
+      # svacols <- c(ncol(dgeObj$DiseaseTreatment_sva)-svobj$n.sv+1, ncol(dgeObj_sva$DiseaseTreatment_sva))
+      # dgeObj$design <- cbind(dgeObj$design, dgeObj_sva$DiseaseTreatment_sva[,svacols[1]:svacols[2]])
+      dgeObj$design <- cbind(dgeObj$design, sv)
 
   } else tsmsg ("No Surrogate Variables Found. DGEobj is unchanged.")
 
--- test/training.R
+++ test/training.R
@@ -3,12 +3,14 @@ library(tidyverse)
 library(DGEobj)
 library(DGE.Tools2)
 library(JRTutil)
+library(Xpress2R)
 
 
 dgeObj <- getRDSobjFromStash("BDL_Rat_LiverSlice_P-20170808-0001_03Dec2017.RDS")
 
 #inspect DGEobj
 inventory(dgeObj)
+showMeta(dgeObj)
 
 #Get various intensity units
 counts <- getItem(dgeObj, "counts")
@@ -17,5 +19,8 @@ geneLength <- getItem(dgeObj, "geneData")[["ExonLength"]]
 tpm <- convertCounts(counts, unit="tpm", geneLength=geneLength)
 log2fpkm <- convertCounts(counts, unit="fpkm", geneLength=geneLength, log=TRUE, normalize="tmm")
 log2cpm <- convertCounts(counts, unit="cpm", log=TRUE, normalize="tmm")
+
+#FPR2 data
+d_fpr2 <- Xpress2DGEO(21353, level="GRCm38ERCC-ensembl91-genes")
 
 
